0184b5ba71fe8cad204269d86e8761a2f65ac607
muhammettalhakaymak23214 <talha.kaymak@bil.omu.edu.tr>
2024-02-26 02:30:18 +0300

    hata düzeltildi

--- garsonapp/lib/sayfalar/giris_sayfasi.dart
+++ garsonapp/lib/sayfalar/giris_sayfasi.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:garsonapp/widgets/my_textbox.dart';
 
 class GirisSayfasi extends StatefulWidget {
   const GirisSayfasi({super.key});
