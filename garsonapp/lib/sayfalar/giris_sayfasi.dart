0b500e66b61a10b29e5284cf75445b85fdfda133
muhammettalhakaymak23214 <talha.kaymak@bil.omu.edu.tr>
2024-02-25 19:54:53 +0300

    main.dart ve giris sayfasi olusturuldu.

---
+++ garsonapp/lib/sayfalar/giris_sayfasi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GirisSayfasi extends StatefulWidget {
+  const GirisSayfasi({super.key});
+
+  @override
+  State<GirisSayfasi> createState() => _GirisSayfasiState();
+}
+
+class _GirisSayfasiState extends State<GirisSayfasi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
